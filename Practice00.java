4208bcf2b167c318cc2ae9f130f817bd0ad16564
pavlusenkode <pavlusenko.de@gmail.com>
2025-03-17 21:01:37 +0100

    Add class

--- Practice00.java
+++ Practice00.java
@@ -1,7 +1,7 @@
 public class Practice00 {
 
     public static void main(String[] args) {
-        System.out.println("Hello good good world!");
+        System.out.println("Hello good good good world!");
     }
 
 }
